89c7c9bd2fc4c05160daaf9c8e8deb811a07096b
scaminom <jcamino7758@uta.edu.ec>
2025-05-20 23:38:36 -0500

    Add login register for microservices

--- auth-service/src/main.ts
+++ auth-service/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Auth service is running on port ${port}`);
 }
 bootstrap();
