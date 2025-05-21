4326f525d00750c47202a0f2ed608e63b4302114
scaminom <jcamino7758@uta.edu.ec>
2025-05-20 21:22:15 -0500

    Add user service and funtionality in roder to work with microservices

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MicroservicesModule } from './microservices/microservices.module';
+import { AuthModule } from './modules/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MicroservicesModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
